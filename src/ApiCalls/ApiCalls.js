516d9c286b32003c617f59fc1fb09358be0ceb9c
Tariq Sherazi <zetariqkhan@gmail.com>
2022-11-25 07:51:56 -0800

    committed

--- src/ApiCalls/ApiCalls.js
+++ src/ApiCalls/ApiCalls.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const baseUrl = "http://localhost:5000";
+// const baseUrl = "http://localhost:5000";
+const baseUrl = "https://api-api.herokuapp.com";
+
 
  export const getDemo=async()=>{
     const {data}=await axios.get(baseUrl+'/demo')
